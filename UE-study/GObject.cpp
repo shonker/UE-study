1d3676180bb83ae79dbf553944df333494d77387
pseuxide <s3pt3mb3r@protonmail.com>
2023-03-07 23:57:53 +0900

    make getTUObjectArray return ObjObjects

--- UE-study/GObject.cpp
+++ UE-study/GObject.cpp
@@ -2,19 +2,16 @@
 
 using namespace std;
 
-auto getTUObjectArray() -> void {
+auto getTUObjectArray() -> TUObjectArray {
 	const char* moduleName = "AtomicHeart-Win64-Shipping.exe";
 	//auto ps = PatternScanner(moduleName, "89 15 ? ? ? ? 85 FF", 2, 0);
 	auto address = find_signature(moduleName, "89 15 ? ? ? ? 85 FF") + 0x2;
 	auto rip_offset = *reinterpret_cast<DWORD*>(address);
 	FUObjectArray* fUObjectArray = reinterpret_cast<FUObjectArray*>(address + sizeof(DWORD) + rip_offset);
 	cout << hex << (uintptr_t)fUObjectArray << endl;
-	TUObjectArray tUObjectArray = fUObjectArray->ObjObjects;
-
-	cout <<  tUObjectArray.MaxElements << endl;
-	cout << hex << reinterpret_cast<uintptr_t>(reinterpret_cast<DWORD*>(&tUObjectArray.MaxElements)) << endl;
+	return fUObjectArray->ObjObjects;
 }
 
 auto getObject() -> void {
-	getTUObjectArray();
+	TUObjectArray tUObject = getTUObjectArray();
 }
